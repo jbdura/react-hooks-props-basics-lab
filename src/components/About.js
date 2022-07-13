4187ee0751f68b22744b321268d7850e493ca10a
jbdura <bidurajohnson@gmail.com>
2022-07-13 23:30:04 +0300

    commit tree 38d0571ca34368ef874a404024b034aff61d302a

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(props) {
+  const bio = props.bio;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {!!bio ? <p>{props.bio}</p> : <div style={{ display: "none" }}></div>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
+
+
 
 export default About;
